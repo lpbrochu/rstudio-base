abcaf2bff97025708f08ad8286befc4b91376052
Louis-Philippe Brochu <lpbrochu@gmail.com>
2018-02-15 04:18:48 -0500

    first try at it

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,3 @@ RUN apt-get install -y zlib1g-dev libssh2-1-dev
 
 ADD install_packages.R /base/install_packages.R
 RUN /usr/local/bin/Rscript /base/install_packages.R
-
--- install_packages.R
+++ install_packages.R
@@ -2,3 +2,6 @@
 install.packages(c("reader", "dplyr", "tidyr"),
                  repos = "http://lib.stat.cmu.edu/R/CRAN", 
                  dependencies = TRUE)
+
+
+pacman::p_load("ggvis", "rsconnect", "RCurl", "RJSONIO", "PKI")
